2994a9acdfea7de8ca40ddf7bd7c042257f6342e
Jared <jared@photosandtext.com>
2013-07-08 07:28:48 -0400

    Played with font colors, made URLs prettier.

--- robodomo.py
+++ robodomo.py
@@ -7,21 +7,21 @@ app.debug = True
 app.config.from_object('settings.dev')
 api = restful.Api(app)
 
-@app.route('/servers')
+@app.route('/servers/')
 def list_data():
     client = apiServersList()
     results = client.get()
     return render_template('list.html', results=results)
 
-@app.route('/servers/<resID>')
+@app.route('/servers/<resID>/')
 def view_resource(resID):
     apiClient = apiServers()
     results = apiClient.get(resID)
     return render_template("servers/view.html", results=results)
 
 
-api.add_resource(apiServers,'/api/servers/<resID>')
-api.add_resource(apiServersList, '/api/servers')
+api.add_resource(apiServers,'/api/servers/<resID>/')
+api.add_resource(apiServersList, '/api/servers/')
 
 if __name__ == '__main__':
     app.run()
