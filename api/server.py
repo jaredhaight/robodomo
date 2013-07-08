d627df04faa56e1cb4fbe483b3b18bcc5a699cc2
Jared <jared@photosandtext.com>
2013-07-07 20:38:42 -0400

    New functions for cleaning up data, reorganized things a bit.

--- api/server.py
+++ api/server.py
@@ -1,20 +1,24 @@
 from flask import request
 from flask.ext.restful import Resource, reqparse, abort
 from mongostore import roboMongoClient
-from utils import sanitize_data, sanitize_stats, organize_stats, validate_stats, validate_server, get_data_from_request
+from utils import sanitize_data, clean_response_data, clean_request_data, organize_stats, validate_stats, validate_server, get_data_from_request
 
 serverClient = roboMongoClient(collection="servers")
 statsClient = roboMongoClient(collection="stats")
 
 class apiServers(Resource):
     def get(self,resID):
-        res = serverClient.find(key="_id", value=resID)
-        statsList = statsClient.find(key="server_id", value=resID)
-        stats = organize_stats(statsList)
-        cleanStats = sanitize_stats(stats)
-        res = sanitize_data(res[0])
-        res["stats"] = cleanStats
-        return res
+        result = serverClient.find(key="_id", value=resID)
+        try:
+            statsList = statsClient.find(key="server_id", value=resID)
+            stats = organize_stats(statsList)
+            cleanStats = sanitize_data(stats)
+        except:
+            cleanStats = None
+        result = clean_response_data(result[0])
+        if cleanStats:
+            result["stats"] = cleanStats
+        return result
 
     def post(self,resID):
         data = get_data_from_request(request)
@@ -23,34 +27,44 @@ class apiServers(Resource):
             if test is not True:
                 return abort(400, message=test)
         data["server_id"] = resID
+        data = clean_request_data(data)
         stats = statsClient.insert(data)
         result = statsClient.find(key="_id", value=str(stats))
-        result = sanitize_data(result[0])
-        return result
+        obj = clean_response_data(result[0])
+        return obj
 
     def put(self,resID):
-        res = serverClient.find(key="_id", value=resID)
-        for formkey, formdata in request.form.iteritems():
-            if formkey in ("_id", "created", "updated"):
-                pass
-            else:
-                res[0][formkey] = formdata
-        obj = serverClient.update(res[0])
-        obj = sanitize_data(obj)
+        result = serverClient.find(key="_id", value=resID)
+        data = get_data_from_request(request)
+        test = validate_server(data)
+        if test is not True:
+            return abort(400, message=test)
+        else:
+            for key, value in data.iteritems():
+                if key in ("_id", "created", "updated"):
+                    pass
+                else:
+                    result[0][key] = value
+        cleanRes = clean_request_data(result[0])
+        obj = serverClient.update(cleanRes)
+        obj = clean_response_data(obj)
         return obj
 
     def delete(self, resID):
         serverClient.delete(resID)
-        return {"Status": "Deleted"}
+        return {"message": "Object deleted"}
 
 class apiServersList(Resource):
     def get(self):
-        servers = serverClient.find()
+        if request.args.get('tag'):
+            servers = serverClient.find(key="tags", value=request.args.get('tag'))
+        else:
+            servers = serverClient.find()
         i = 0
         d = []
         for server in servers:
             i = i + 1
-            server = sanitize_data(server)
+            server = clean_response_data(server)
             d.append(server)
         results = {"count":i}
         results.update({"servers":d})
@@ -61,6 +75,7 @@ class apiServersList(Resource):
         test = validate_server(data)
         if test is not True:
             return abort(400, message=test)
+        data = clean_request_data(data)
         server = serverClient.insert(data)
         result = serverClient.find(key="_id", value=str(server))
         result = sanitize_data(result[0])
--- api/utils.py
+++ api/utils.py
@@ -1,22 +1,39 @@
+from flask import request
+from bson import ObjectId
 import datetime
+valid_server_keys = ("_id","name","type","tags")
 
-def sanitize_data(res):
-    res['_id'] = str(res['_id'])
-    res['date_created'] = res['date_created'].strftime("%Y-%m-%d %H:%M:%S")
-    try:
-        res['updated'] = res['updated'].strftime("%Y-%m-%d %H:%M:%S")
-    except:
-        return res
+def clean_response_data(res):
+    for key, value in res.iteritems():
+        if isinstance(key, datetime.datetime):
+            stringKey = key.strftime("%Y-%m-%d %H:%M:%S")
+            res[stringKey] = value
+            del(res[key])
+        if isinstance(value, datetime.datetime):
+            res[key] = value.strftime("%Y-%m-%d %H:%M:%S")
+        if isinstance(value, ObjectId):
+            res[key] = str(value)
     return res
 
-def sanitize_stats(stats):
-    for stat in stats:
-        for key, value in stats[stat].iteritems():
+def clean_request_data(data):
+    try:
+        data["tags"] = data["tags"].split()
+    except:
+        pass
+    return data
+
+def sanitize_data(res):
+    for obj in res:
+        for key, value in res[obj].iteritems():
             if isinstance(key, datetime.datetime):
                 stringKey = key.strftime("%Y-%m-%d %H:%M:%S")
-                stats[stat][stringKey] = value
-                del(stats[stat][key])
-    return stats
+                res[obj][stringKey] = value
+                del(res[obj][key])
+            if isinstance(value, datetime.datetime):
+                res[obj][key] = value.strftime("%Y-%m-%d %H:%M:%S")
+            if isinstance(value, ObjectId):
+                res[obj][key] = str(value)
+    return res
 
 def organize_stats(statsList):
     #Get a list of unique things we've monitored
@@ -50,14 +67,18 @@ def validate_stats(key, value):
     return True
 
 def validate_server(data):
-    try:
-        data["name"]
-    except:
-        return "Need name"
-    try:
-        data["type"]
-    except:
-        return "Need type"
+    if request.method == "POST":
+        try:
+            data["name"]
+        except:
+            return "Need name"
+        try:
+            data["type"]
+        except:
+            return "Need type"
+    for key, value in data.iteritems():
+        if key not in valid_server_keys:
+            return "%s is not a recognized key for server" % (key)
     return True
 
 def get_data_from_request(request):
